4e3409b4fef86f0624e68cb50a35efa5bae990f9
Fausto Cortez <faustocortez1994@gmail.com>
2021-11-03 14:57:07 -0600

    Using the custom hook to get images from giphy

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,30 +1,20 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
 // import { getGifs } from '../helpers/getGifs';
-// import { GifGridItem } from './GifGridItem';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    // const [images, setImages] = useState([]);
-    const { loading} = useFetchGifs();
-
-    // component did mount in react classes
-    // the second parameter "[]" is a list of dependencies
-    // if [] equal to one execution
-    // useEffect(() => {
-    //     getGifs(category)
-    //         // como el argumente del then es el mismo mandado en la función
-    //         // interna se usa shortand la función interna de manera directa.
-    //         // antes: then(imgs => setImages(imgs))
-    //         .then(setImages);
-    // }, [ category ]);
+    const { data: images, loading} = useFetchGifs(category);
 
     return (
-        <>
+        <> 
             <h3>{ category }</h3>
 
-            { loading ? 'Cargando...' : 'Listo'}
-            {/* <div className="card-grid">
+            {/* condicion lógica AND */}
+            { loading && <p>Loading...</p> }
+
+            <div className="card-grid">
                 { 
                     images.map(img => (
                         <GifGridItem
@@ -33,7 +23,7 @@ export const GifGrid = ({ category }) => {
                         />
                     ))
                 }
-            </div> */}
+            </div>
         </>
     )
 }
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,19 +1,39 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { getGifs } from "../helpers/getGifs";
 
-export const useFetchGifs = () => {
+export const useFetchGifs = ( category ) => {
     
     const [state, setState] = useState({
         data: [],
         loading: true,
     });
 
-    setTimeout(() => {
-        setState({
-            data: [1, 2, 3, 4, 5, 6, 7],
-            loading: false
-        });
-    }, 3000);
+    useEffect(() => {
+
+        getGifs(category)
+            .then(imgs => {
+                setTimeout(() => {
+                    setState({
+                        data: imgs,
+                        loading: false,
+                    });
+                }, 1500);
+            })
+
+    }, [category])
 
     return state;
 }
 
+// component did mount in react classes
+// the second parameter "[]" is a list of dependencies
+// if [] equal to one execution
+// useEffect(() => {
+//     getGifs(category)
+//         // como el argumente del then es el mismo mandado en la función
+//         // interna se usa shortand la función interna de manera directa.
+//         // antes: then(imgs => setImages(imgs))
+//         .then(setImages);
+// }, [ category ]);
+
+// ** LOS EFECTOS NO PUEDEN SER ASYNC PORQUE ESPERAN ALGO SÍNCRONO.
